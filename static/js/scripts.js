2940c292acf4b008ce1ea64712f6e4a9241ec01b
goormany <zarbot351@yandex.ru>
2021-05-10 20:55:53 +0300

    BootStarp v1.1 {Плохой вариант}

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Bare v4.3.0 (https://startbootstrap.com/template/bare)
+* Start Bootstrap - The Big Picture v4.3.0 (https://startbootstrap.com/template/the-big-picture)
 * Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
